ab84d5c1c9ebe7e69bb874a0d8093eac6d8d8847
Alwin <meijboom.am@gmail.com>
2023-01-25 12:00:48 +0100

    Add JSON support

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,9 @@
 #include <SFML/Graphics.hpp>
 #include "Game.hpp"
 #include "UI/Button.hpp"
+#include <fstream>
+#include <nlohmann/json.hpp>
+using json = nlohmann::json;
 
 void toFight(Core::Event event) {
     event.game->activate("Fight");
@@ -16,41 +19,51 @@ void close(Core::Event event) {
 }
 
 int main() {
-    sf::RenderWindow window(sf::VideoMode(1280, 756), "Omega");
-    window.setVerticalSyncEnabled(true);
+//    sf::RenderWindow window(sf::VideoMode(1280, 756), "Omega");
+//    window.setVerticalSyncEnabled(true);
+//
+//    Core::Game game(&window);
+//
+//    sf::Font font;
+//    font.loadFromFile("assets/RulerGold.ttf");
+//    sf::Text playText("Play", font, 24);
+//    playText.setFillColor(sf::Color::Black);
+//
+//    sf::Text menuText("Menu", font, 24);
+//    menuText.setFillColor(sf::Color::Black);
+//
+//    sf::Text closeText("Quit", font, 24);
+//    closeText.setFillColor(sf::Color::Black);
+//
+//    UI::Button fightButton(sf::Vector2f(100, 50), sf::Vector2f(500,500), sf::Color::White, playText, &toFight);
+//    UI::Button menuButton(sf::Vector2f(100, 50), sf::Vector2f(500,500), sf::Color::White, menuText, &toMenu);
+//    UI::Button quitButton(sf::Vector2f(100, 50), sf::Vector2f(0,0), sf::Color::White, closeText, &close);
+//
+//    Core::Scene menu(&game, std::string("Menu"));
+//    Core::Scene fight(&game, std::string("Fight"));
+//
+//    menu.addObject(&fightButton);
+//    menu.addObject(&quitButton);
+//    fight.addObject(&menuButton);
+//    std::vector<Core::Scene*> scenes;
+//
+//    scenes.push_back(&menu);
+//    scenes.push_back(&fight);
+//
+//    game.addScenes(scenes);
+//    game.activate("Menu");
+//
+//    game.run();
 
-    Core::Game game(&window);
+    std::ifstream f("assets/gamedata.json");
+    json data = json::parse(f);
 
-    sf::Font font;
-    font.loadFromFile("assets/RulerGold.ttf");
-    sf::Text playText("Play", font, 24);
-    playText.setFillColor(sf::Color::Black);
+    for (auto highscore : data["highscores"]) {
+        std::string name = highscore["name"];
+        int score = highscore["score"];
 
-    sf::Text menuText("Menu", font, 24);
-    menuText.setFillColor(sf::Color::Black);
-
-    sf::Text closeText("Quit", font, 24);
-    closeText.setFillColor(sf::Color::Black);
-
-    UI::Button fightButton(sf::Vector2f(100, 50), sf::Vector2f(500,500), sf::Color::White, playText, &toFight);
-    UI::Button menuButton(sf::Vector2f(100, 50), sf::Vector2f(500,500), sf::Color::White, menuText, &toMenu);
-    UI::Button quitButton(sf::Vector2f(100, 50), sf::Vector2f(0,0), sf::Color::White, closeText, &close);
-
-    Core::Scene menu(&game, std::string("Menu"));
-    Core::Scene fight(&game, std::string("Fight"));
-
-    menu.addObject(&fightButton);
-    menu.addObject(&quitButton);
-    fight.addObject(&menuButton);
-    std::vector<Core::Scene*> scenes;
-
-    scenes.push_back(&menu);
-    scenes.push_back(&fight);
-
-    game.addScenes(scenes);
-    game.activate("Menu");
-
-    game.run();
+        printf("Name: %s | Score: %d\n", name.c_str(), score);
+    }
 
     return 0;
 }
